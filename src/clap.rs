a1d9278c89c23bc6c135ff6c00e9c089238cd09b
Luó <145170458+luoshanyuan@users.noreply.github.com>
2024-05-05 18:52:31 +1000

    added parser clap with verbosity levels 0, 1, 2

---
+++ src/clap.rs
@@ -0,0 +1,51 @@
+use clap::{arg, command, Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[derive(Debug)]
+pub struct CommandLines {
+    #[command(subcommand)]
+    pub subcommand: SubCommands,
+    /// Increase logging verbosity
+    #[arg(short('v'), long, action = clap::ArgAction::Count)]
+    pub verbosity: u8,
+    /// Enable debug output, another way to increase logging verbosity
+    #[arg(
+        long = "debug",
+        env = "CRATE_DEBUG",
+        value_name = "boolean",
+        default_value_t = false
+    )]
+    pub debug: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct ServeArgs {
+    /// IP address to listen on
+    #[arg(
+        short('i'),
+        long = "ip",
+        env = "API_IP",
+        value_name = "address",
+        default_value = "0.0.0.0"
+    )]
+    pub listener_ip: String,
+
+    /// TCP port to listen on
+    #[arg(
+        short('p'),
+        long = "port",
+        env = "API_PORT",
+        value_name = "tcp",
+        default_value_t = 8000
+    )]
+    pub listener_port: u16,
+
+    /// Test printing error message
+    #[arg(
+        short('t'),
+        long = "test-error",
+        value_name = "boolean",
+        default_value_t = false
+    )]
+    pub test_err: bool,
+}
